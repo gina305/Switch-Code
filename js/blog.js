26d05afc93468d530cbc0a06051798a5f01668fd
Gina <5680013+gina305@users.noreply.github.com>
2023-03-25 16:31:22 +0000

    successfully established working code.

--- js/blog.js
+++ js/blog.js
@@ -1,29 +1,35 @@
-const axios = require('axios');
+const Airtable = require('airtable');
 
 // Replace <YOUR_API_KEY> with your personal access token
 const apiKey = "keyUKafa60cDwHmoq";
 
-  // Replace <YOUR_BASE_ID> with your Airtable base ID
-const baseId = "app32VEPUDOKJwj8";
+// Replace <YOUR_BASE_ID> with your Airtable base ID
+const baseId = "app32VEPUDOKJwj8t";
 
-  // Replace <YOUR_TABLE_NAME> with the name of the Airtable table you want to retrieve records from
-const tableName = "tbldAB3RXAMBjUiQ9";
-  // Initialize Airtable API endpoin
+// Replace <YOUR_TABLE_NAME> with the name of the Airtable table you want to retrieve records from
+const tableName = "tblxIhDyoscjELMJm";
+
+// Initialize Airtable client
+const base = new Airtable({ apiKey: apiKey }).base(baseId);
+
+// Retrieve records from Airtable
+base(tableName).select({
+  maxRecords: 10,
+  view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+  // Iterate through records
+  records.forEach(function(record) {
+
+    console.log(record.get('Name'));
 
 
-// Initialize Airtable API endpoint
-const endpoint = `https://api.airtable.com/v2/${baseId}/${tableName}`;
+  });
 
-// Make HTTP request to Airtable API to retrieve records
-axios.get(endpoint, {
-  headers: {
-    "Authorization": `Bearer ${apiKey}`
+  // Fetch the next page of records
+  fetchNextPage();
+}, function done(err) {
+  if (err) {
+    console.error(err);
+    return;
   }
-})
-.then(response => {
-  const records = response.data.records;
-  console.log(records);
-})
-.catch(error => {
-  console.log(error);
 });
