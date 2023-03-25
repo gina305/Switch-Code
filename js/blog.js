211eda0d777879ba487d51ca6fcc84de49f50ffc
Gina <5680013+gina305@users.noreply.github.com>
2023-03-25 19:44:00 +0000

    updated js git add .
    git commit -m updated

--- js/blog.js
+++ js/blog.js
@@ -15,8 +15,6 @@ const base = new Airtable({ apiKey: apiKey }).base(baseId);
 // Lisrt of ads
 const jobAds = []
 
-// Target
-const jobCardContainer = document.getElementById("job-cards");
 
 
 // Retrieve records from Airtable
@@ -45,22 +43,7 @@ base(tableName).select({
     
     
     if(status == "Yes"){
-      
-        jobAds.push(myObj);
-        console.log(jobAds);
-
-        const jobCard = document.createElement("div");
-      jobCard.classList.add("col");
-      jobCard.innerHTML = `
-        <div class="card border-0 shadow-sm">
-          <div class="card-body">
-            <h5 class="card-title">${job.name}</h5>
-            <p class="card-text">${job.subtext}</p>
-            <a href={job.link} class="btn btn-primary">Apply Now</a>
-          </div>
-        </div>
-      `;
-      jobCardContainer.appendChild(jobCard);
+      console.log(name);
 
     }
   
