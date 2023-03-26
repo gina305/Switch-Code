9e26bfbffdb2ee271ebe54bd846c5b50a2d62f7c
Gina <5680013+gina305@users.noreply.github.com>
2023-03-26 02:47:55 +0000

    checking functionalify for blog cards

--- js/airtable.js
+++ js/airtable.js
@@ -1,24 +1,24 @@
 
-const base = new Airtable({ apiKey: 'keyUKafa60cDwHmoq' }).base('app32VEPUDOKJwj8t');
+// const base = new Airtable({ apiKey: 'keyUKafa60cDwHmoq' }).base('app32VEPUDOKJwj8t');
 
-base('tblxIhDyoscjELMJm').select({
-  // Add any additional filtering or sorting options here
-}).eachPage((records, fetchNextPage) => {
-  // Process each page of records
-  records.forEach(record => {
-    // Create a new article element for each record
-    const article = document.createElement('article');
-    article.innerHTML = `
-      <h2>${record.get('Title')}</h2>
-      <p>${record.get('Publish Date')}</p>
-      <!-- Add any additional fields from your Airtable table -->
-    `;
-    // Add the article to the container element
-    document.getElementById('blog-articles').appendChild(article);
-  });
+// base('tblxIhDyoscjELMJm').select({
+//   // Add any additional filtering or sorting options here
+// }).eachPage((records, fetchNextPage) => {
+//   // Process each page of records
+//   records.forEach(record => {
+//     // Create a new article element for each record
+//     const article = document.createElement('article');
+//     article.innerHTML = `
+//       <h2>${record.get('Title')}</h2>
+//       <p>${record.get('Publish Date')}</p>
+//       <!-- Add any additional fields from your Airtable table -->
+//     `;
+//     // Add the article to the container element
+//     document.getElementById('blog-articles').appendChild(article);
+//   });
 
-  // Fetch the next page of records, if there are more
-  fetchNextPage();
-}, error => {
-  console.error(error);
-});
+//   // Fetch the next page of records, if there are more
+//   fetchNextPage();
+// }, error => {
+//   console.error(error);
+// });
--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
-import { getBlogPosts } from './airtable.js';
+// import { getBlogPosts } from './airtable.js';
 
-const searchBox = document.getElementById('search-box');
-const blogCards = document.getElementById('blog-cards');
+// const searchBox = document.getElementById('search-box');
+// const blogCards = document.getElementById('blog-cards');
 
-function renderBlogCards(posts) {
-  blogCards.innerHTML = '';
+// function renderBlogCards(posts) {
+//   blogCards.innerHTML = '';
 
-  posts.forEach(post => {
-    const card = document.createElement('div');
-    card.classList.add('blog-card');
-    card.innerHTML = `
-      <h2>${post.get('Title')}</h2>
-      <p>${post.get('Publish Date')}</p>`;
-      blogCards.appendChild(card);
-    });
-  }
+//   posts.forEach(post => {
+//     const card = document.createElement('div');
+//     card.classList.add('blog-card');
+//     card.innerHTML = `
+//       <h2>${post.get('Title')}</h2>
+//       <p>${post.get('Publish Date')}</p>`;
+//       blogCards.appendChild(card);
+//     });
+//   }
   
-  function filterBlogCards(posts, query) {
-  return posts.filter(post => {
-  const title = post.get('Title').toLowerCase();
-  const author = post.get('Author').toLowerCase();
-  const content = post.get('Content').toLowerCase();
-  return title.includes(query) || author.includes(query) || content.includes(query);
-  });
-  }
+//   function filterBlogCards(posts, query) {
+//   return posts.filter(post => {
+//   const title = post.get('Title').toLowerCase();
+//   const author = post.get('Author').toLowerCase();
+//   const content = post.get('Content').toLowerCase();
+//   return title.includes(query) || author.includes(query) || content.includes(query);
+//   });
+//   }
   
-  async function init() {
-  try {
-  const posts = await getBlogPosts();
-  renderBlogCards(posts);
+//   async function init() {
+//   try {
+//   const posts = await getBlogPosts();
+//   renderBlogCards(posts);
 
-  searchBox.addEventListener('input', () => {
-    const query = searchBox.value.trim().toLowerCase();
-    const filteredPosts = filterBlogCards(posts, query);
-    renderBlogCards(filteredPosts);
-  });
-} catch (error) {
-  console.error(error);
-  }
-  }
+//   searchBox.addEventListener('input', () => {
+//     const query = searchBox.value.trim().toLowerCase();
+//     const filteredPosts = filterBlogCards(posts, query);
+//     renderBlogCards(filteredPosts);
+//   });
+// } catch (error) {
+//   console.error(error);
+//   }
+//   }
   
-  init();
+//   init();
