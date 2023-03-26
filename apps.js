ea7c9f7a3b0d79a1325a3a4f6fe22f958edb7998
Gina <5680013+gina305@users.noreply.github.com>
2023-03-26 04:44:50 +0000

    updated js~[3~

---
+++ apps.js
@@ -0,0 +1,40 @@
+const searchBox = document.getElementById('searchBox');
+const resultsContainer = document.getElementById('resultsContainer');
+
+const airtableApiKey = 'keyUKafa60cDwHmoq';
+const airtableBaseId = 'app32VEPUDOKJwj8t';
+const airtableTableName = 'tblxIhDyoscjELMJm';
+
+const airtableApiUrl = `https://api.airtable.com/v0/${airtableBaseId}/${airtableTableName}`;
+
+searchBox.addEventListener('input', async () => {
+    const searchTerm = searchBox.value.trim();
+    resultsContainer.innerHTML = '';
+
+    if (searchTerm.length > 0) {
+        const records = await fetchAirtableRecords(searchTerm);
+        renderAirtableRecords(records);
+    }
+});
+
+async function fetchAirtableRecords(searchTerm) {
+    const response = await axios.get(`${airtableApiUrl}?filterByFormula=OR(FIND("${searchTerm}", {Field 1}), FIND("${searchTerm}", {Field 2}))`, {
+        headers: {
+            'Authorization': `Bearer ${airtableApiKey}`
+        }
+    });
+
+    return response.data.records;
+}
+
+function renderAirtableRecords(records) {
+    records.forEach(record => {
+        const resultDiv = document.createElement('div');
+        resultDiv.classList.add('result');
+        resultDiv.innerHTML = `
+            <h2>${record.fields['Title']}</h2>
+            <p>${record.fields['Publish Date']}</p>
+        `;
+        resultsContainer.appendChild(resultDiv);
+    });
+}
