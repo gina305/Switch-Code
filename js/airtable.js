d66b37ac4139ded030318c2b54f3407232bad422
Gina <5680013+gina305@users.noreply.github.com>
2023-03-26 02:34:39 +0000

    checking functionalify for blog cards

--- js/airtable.js
+++ js/airtable.js
@@ -1,16 +1,24 @@
 const Airtable = require('airtable');
 const base = new Airtable({ apiKey: 'keyUKafa60cDwHmoq' }).base('app32VEPUDOKJwj8t');
 
-function getBlogPosts() {
-  return new Promise((resolve, reject) => {
-    base('tblxIhDyoscjELMJm').select({
-      // Add any additional filtering or sorting options here
-    }).all((error, records) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(records);
-      }
-    });
+base('tblxIhDyoscjELMJm').select({
+  // Add any additional filtering or sorting options here
+}).eachPage((records, fetchNextPage) => {
+  // Process each page of records
+  records.forEach(record => {
+    // Create a new article element for each record
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <h2>${record.get('Title')}</h2>
+      <p>${record.get('Publish Date')}</p>
+      <!-- Add any additional fields from your Airtable table -->
+    `;
+    // Add the article to the container element
+    document.getElementById('blog-articles').appendChild(article);
   });
-}
+
+  // Fetch the next page of records, if there are more
+  fetchNextPage();
+}, error => {
+  console.error(error);
+});
