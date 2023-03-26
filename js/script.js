1fd2972f94bd84d68d0eb499867c8c9a00053e95
Gina <5680013+gina305@users.noreply.github.com>
2023-03-26 02:40:54 +0000

    checking functionalify for blog cards

--- js/script.js
+++ js/script.js
@@ -11,6 +11,33 @@ function renderBlogCards(posts) {
     card.classList.add('blog-card');
     card.innerHTML = `
       <h2>${post.get('Title')}</h2>
-      <p>By ${post.get('Author')}</p>
-      <p>${post.get('Date')}</p>
-      <p>${post.get
+      <p>${post.get('Publish Date')}</p>`;
+      blogCards.appendChild(card);
+    });
+  }
+  
+  function filterBlogCards(posts, query) {
+  return posts.filter(post => {
+  const title = post.get('Title').toLowerCase();
+  const author = post.get('Author').toLowerCase();
+  const content = post.get('Content').toLowerCase();
+  return title.includes(query) || author.includes(query) || content.includes(query);
+  });
+  }
+  
+  async function init() {
+  try {
+  const posts = await getBlogPosts();
+  renderBlogCards(posts);
+
+  searchBox.addEventListener('input', () => {
+    const query = searchBox.value.trim().toLowerCase();
+    const filteredPosts = filterBlogCards(posts, query);
+    renderBlogCards(filteredPosts);
+  });
+} catch (error) {
+  console.error(error);
+  }
+  }
+  
+  init();
