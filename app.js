58245cc43a20f8446dd747b30a9fc14fef1801db
Gina <5680013+gina305@users.noreply.github.com>
2023-03-26 18:49:04 +0000

    updates

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+// require("dotenv").config();
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
  export default function foo() {
   const apiKey = process.env.KEY; // replace with your Airtable API key
